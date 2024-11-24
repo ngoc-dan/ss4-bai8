6f324dfd28fd961ba035fb7c0a2028ad0c296ad5
ngoc-dan <ngocdan191026@gmail.com>
2024-11-24 23:18:38 +0700

    Add files via upload

---
+++ 4.8.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int a,b,c;
+	printf("nhap cac so a,b,c ");
+	scanf("%d""%d""%d" , &a,&b,&c);
+	if(a+b>c,a+c>b,b+c>a){
+		printf("la hinh tam giac");
+	}
+	else{
+		printf("kh phai hinh tam giac");
+		
+	}
+	
+	return 0;
+	}
